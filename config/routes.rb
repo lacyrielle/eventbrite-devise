7a979dc3ea4435db807ec34d3b71ca64edac49c4
cyrielle <lacyrielle@gmail.com>
2020-05-12 23:06:21 +0200

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'event/index'
-  get 'event/new'
+  devise_for :users
+  get 'home', to: 'event#index'
+  get 'new', to: 'event#new'
   get 'static_pages/index', to: 'static_pages#index'
   get 'static_pages/secret'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
